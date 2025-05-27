8650d577aa29786ac627a94c744efbb2573e956e
Sherwin Arellano <s.arellano@esis-italia.com>
2025-05-27 09:16:35 +0200

    fix: Empty context params

--- core/messages/base.py
+++ core/messages/base.py
@@ -4,11 +4,14 @@ from core.utils import CallContext
 from dataclasses import dataclass
 
 
-@dataclass
 class MessageContext(CallContext):
-    ti: TrackInfo
-    point: tuple[float, float]
-    epoch_s: float | None = None
+    def __init__(
+        self, ti: TrackInfo, point: tuple[float, float], epoch_s: float | None = None
+    ):
+        super().__init__()
+        self.ti = ti
+        self.point = point
+        self.epoch_s = epoch_s
 
 
 class MessageBuilder(ABC):
