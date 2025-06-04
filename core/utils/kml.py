2e9868b302ffc84f41123ba2852a61d7d908f602
Sherwin Arellano <s.arellano@esis-italia.com>
2025-06-04 11:46:54 +0200

    feat: Implement track number increment

--- core/utils/kml.py
+++ core/utils/kml.py
@@ -1,11 +1,22 @@
 from lxml import etree as ET
 from lxml.etree import CDATA
+from core.models import TrackInfo
 import re
 
 _NS = {"k": "http://www.opengis.net/kml/2.2"}
 _RE = re.compile(r"^([A-Za-z0-9]+-)(\d+)(.*)")
 
 parser = ET.XMLParser(remove_blank_text=True)
+
+
+def _increment_name_if_exists(name: str) -> str | None:
+    match = _RE.match(name)
+    if not match:
+        return None
+
+    prefix, number_str, suffix = match.groups()
+    new_number = int(number_str) + 1
+    return f"{prefix}{new_number}{suffix}"
 
 
 def increment_all_track_numbers(path: str, output_path: str):
@@ -24,19 +35,36 @@ def increment_all_track_numbers(path: str, output_path: str):
 
         # lxml wants explicit None‐check hence `if name_el is not None`
         old_name = (name_el if name_el is not None else pm_name_el).text or ""
-        match = _RE.match(old_name)
-        if not match:
+        new_name = _increment_name_if_exists(old_name)
+        if not new_name:
             continue
-
-        prefix, number_str, suffix = match.groups()
-        new_number = int(number_str) + 1
-        new_name = CDATA(f"{prefix}{new_number}{suffix}")
 
         # Ships have their own layer
         if name_el is not None and _RE.match(name_el.text or ""):
-            name_el.text = new_name
+            name_el.text = CDATA(new_name)
 
         # While driving routes (trucks) only have one layer
-        pm_name_el.text = new_name
+        pm_name_el.text = CDATA(new_name)
 
     tree.write(output_path, encoding="utf-8", xml_declaration=True, pretty_print=True)
+
+
+def increment_track_number(ti: TrackInfo):
+    tree = ET.parse(ti.path, parser)
+    root: ET._Element = tree.getroot()
+
+    matches = root.xpath(
+        ".//k:name[starts-with(text(), $name)]", namespaces=_NS, name=ti.name
+    )
+
+    for name_el in matches:
+        print(f"[increment] {ti.name}: {name_el.text}")
+        new_name = _increment_name_if_exists(name_el.text)
+
+        if not new_name:
+            continue
+
+        ti.name = new_name
+        name_el.text = CDATA(new_name)
+
+    tree.write(ti.path, encoding="utf-8", xml_declaration=True, pretty_print=True)
