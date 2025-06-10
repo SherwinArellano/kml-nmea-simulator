96a36f40a83cd07b47fb27e675092e7d753e40ca
Sherwin Arellano <s.arellano@esis-italia.com>
2025-06-10 10:37:18 +0200

    feat: Support ws/wss host for MQTT

--- core/transports/mqtt.py
+++ core/transports/mqtt.py
@@ -1,14 +1,31 @@
 from .base import Transport
-from typing import override
+from typing import override, cast
 import paho.mqtt.client as mqtt
 from core.config import AppConfig
+from urllib.parse import urlparse
 
 
 class MQTTTransport(Transport):
+    def _init_ws_client(self, url):
+        parsed = urlparse(url)
+        host = cast(str, parsed.hostname)
+        port = parsed.port or (443 if parsed.scheme == "wss" else 80)
+        path = parsed.path or "/"
+
+        self.client = mqtt.Client(transport="websockets")
+        if parsed.scheme == "wss":
+            self.client.tls_set()
+
+        self.client.ws_set_options(path=path)
+        self.client.connect(host, port)
+
     def __init__(self, broker: tuple[str, int], topic: str):
-        self.client = mqtt.Client()
-        self.client.connect(*broker)
         self.topic = topic
+        if broker[0].startswith(("ws", "wss")):
+            self._init_ws_client(broker[0])
+        else:
+            self.client = mqtt.Client()
+            self.client.connect(*broker)
 
     @override
     def send(self, ctx):
